7c2fa2e9c49b75aecf112577e42a9d376c91418a
Alx <alxvss-crypto@protonmail.com>
2022-02-12 13:47:01 +0100

    patch

--- root/home/alx/.config/qtile/autostart.sh
+++ root/home/alx/.config/qtile/autostart.sh
@@ -5,6 +5,6 @@ xinput set-prop 'MSFT0001:02 06CB:7F28 Touchpad' 'libinput Natural Scrolling Ena
 xinput set-prop 'MSFT0001:02 06CB:7F28 Touchpad' 'libinput Tapping Enabled' 1 &
 xautolock -detectsleep -time 15 -locker "i3lock -i /usr/share/backgrounds/archlinux/awesome.png" &
 #nm-applet &
-#blueberry-tray &
+blueberry-tray &
 #cmst &
-connman-gtk &
+#connman-gtk &
